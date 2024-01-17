191def3ea808caadca8102437d0e115aaab9bb48
Heaven <cutiefemdom@gmail.com>
2024-01-17 20:27:51 +0100

    Fixed rss file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-site: 'https://effortless-heliotrope-703f1f.netlify.app',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: netlify()
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
+
+
