fdf46b0ad3b2185026dddf1f0c27313ebdf21644
Heaven <cutiefemdom@gmail.com>
2024-01-17 20:22:39 +0100

    testing build on netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+site: 'https://effortless-heliotrope-703f1f.netlify.app',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
-
-
