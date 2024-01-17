409462c9a01737f9ce4b234e1b07a57668d75fde
Heaven <cutiefemdom@gmail.com>
2024-01-17 20:07:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: netlify(),
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
